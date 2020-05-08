5ce5c7cc48311e26896d3f699e7e5ffd5327ab28
ViktorNova <viktor@pinknoiseboys.com>
2020-05-08 16:34:08 -0700

    Finding Mixxx title works!

--- mixxx-now-playing.sh
+++ mixxx-now-playing.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-TMPFILE="~/mixxx-now-playing.txt"
+TMPFILE=~/mixxx-now-playing.txt
+touch $TMPFILE
+echo "+"  > $TMPFILE
+while true; do
 
-while pidof mixxx; do
-xdotool search --name Mixxx getwindowname > $TMPFILE
-sleep 5
+	while pidof mixxx; do
+	xdotool search --name "\| Mixxx" getwindowname > $TMPFILE
+	sleep 5
+	done
+
+sleep 60
 done
