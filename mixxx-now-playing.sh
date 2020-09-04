049b15b1f2aa42cd2789c82d693b45f2324cb541
ViktorNova <viktor@pinknoiseboys.com>
2020-09-04 16:44:29 -0700

    Convert output to all uppercase

--- mixxx-now-playing.sh
+++ mixxx-now-playing.sh
@@ -12,12 +12,12 @@ while true; do
 	while pgrep mixxx > /dev/null; do
 
 	if [ $OS == "Linux" ]; then
-		xdotool search --name "\| Mixxx" getwindowname |cut -d\| -f1 | sed 's/,/ -/' > $TXTFILE
+		xdotool search --name "\| Mixxx" getwindowname |cut -d\| -f1 | sed 's/,/ -/' | awk '{ print toupper($0) }'> $TXTFILE
 	elif [ $OS == "Darwin" ]; then
 		python -c " 
 import Quartz
 print(Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListExcludeDesktopElements|Quartz.kCGWindowListOptionOnScreenOnly,Quartz.kCGNullWindowID))
-" | grep "| Mixxx" | cut -d'"' -f 2 |cut -d\| -f1 | sed 's/,/ -/' > $TXTFILE
+" | grep "| Mixxx" | cut -d'"' -f 2 |cut -d\| -f1 | sed 's/,/ -/' | awk '{ print toupper($0) }' > $TXTFILE
 	fi
 
 	sleep 5
