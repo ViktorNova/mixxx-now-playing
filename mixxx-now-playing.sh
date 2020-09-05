c57b804e24b27ab2692091f34d506ec3f482dbdd
Viktor Nova <n0va@darkPhant0m.local>
2020-09-04 19:45:08 -0700

    Ignore case looking for mixxx process this make is cross-platformy

--- mixxx-now-playing.sh
+++ mixxx-now-playing.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-TXTFILE=~/SHARED/mixxx-now-playing.txt
+TXTFILE=~/mixxx-now-playing.txt
 
 touch $TXTFILE
+# TODO: fail here if the file is not writeable
+
 echo " "  > $TXTFILE
 OS=`uname`
 
 echo "Detected OS: $OS"
 
 while true; do
-	while pgrep mixxx > /dev/null; do
+	while pgrep -i mixxx > /dev/null; do
 
 	if [ $OS == "Linux" ]; then
 		xdotool search --name "\| Mixxx" getwindowname |cut -d\| -f1 | sed 's/,/ -/' | awk '{ print toupper($0) }'> $TXTFILE
@@ -20,8 +22,11 @@ print(Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListExcludeDesktopElemen
 " | grep "| Mixxx" | cut -d'"' -f 2 |cut -d\| -f1 | sed 's/,/ -/' | awk '{ print toupper($0) }' > $TXTFILE
 	fi
 
+	# TODO: don't write the file if the value is the same (better for disk I/O)
+	# TODO: unify write command for both OSs
 	sleep 5
 	done
 
+rm -f $TXTFILE
 sleep 60
 done
