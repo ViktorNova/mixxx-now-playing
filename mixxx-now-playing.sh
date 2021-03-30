9d8572afc4c34f7c19d3b8f24f4b5f68e2eb4e8b
kiisu d'salyss <kiisu@dsalyss.com>
2021-03-29 21:21:22 -0700

    update to deal with new songs added

--- mixxx-now-playing.sh
+++ mixxx-now-playing.sh
@@ -12,10 +12,14 @@ echo "Detected OS: $OS"
 #TODO switch out the db path depending on OS
 
 while true; do
+	PLID=$(sqlite3 ~/Library/Application\ Support/Mixxx/mixxxdb.sqlite "select playlist_id from PlaylistTracks pt where id = (select max(id) from PlaylistTracks)")
+	echo "Using Playlist ID: "$PLID
 	while pgrep -i mixxx > /dev/null; do
-		sqlite3 ~/Library/Application\ Support/Mixxx/mixxxdb.sqlite "select library.artist, library.title, library.year from library where library.id = (select PlaylistTracks.track_id from PlaylistTracks where id = (select max(id) from PlaylistTracks));" |
+		sqlite3 ~/Library/Application\ Support/Mixxx/mixxxdb.sqlite "SELECT library.artist, library.title, library.year  FROM "main"."library" WHERE id = (SELECT track_id from PlaylistTracks WHERE playlist_id = $PLID ORDER BY position DESC limit 1)" |
 		sed 's/$/|/' |
-		sed 's/|/ | /g' > $TXTFILE
+		sed 's/|/ | /g' |
+		sed 's/|  |/|/g' |
+		sed -e '1s/^/NOW PLAYING: /' > $TXTFILE
 	sleep 5
 	done
 
